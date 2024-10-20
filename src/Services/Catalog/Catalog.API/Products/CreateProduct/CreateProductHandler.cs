d521aa01cac65185167c4a114d26b329a5630642
Umang Chauhan <umang4191@outlook.com>
2024-10-20 23:18:41 +0200

    Injected IDocumentSession directly in CommandHandler and saving product in database

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,11 +1,17 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using Marten;
+
+namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
     : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+// Injected IDocumentSession directly in the CommandHandler class using Primary Constructor feature
+// Because IDocumentSession is already an abstraction of database operations.So, we don't need any additional
+// abstractions or unnecessary code like repository pattern.
+internal class CreateProductCommandHandler(IDocumentSession session) 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -19,8 +25,10 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
             Price = command.Price
         };
         // Save the entity in the database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         // Return the result i.e.CreateProductResult
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
