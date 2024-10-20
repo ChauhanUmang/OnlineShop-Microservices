caa15d32b75aaa1efe50d04d9cce38d1c37e633b
Umang Chauhan <umang4191@outlook.com>
2024-10-20 21:58:07 +0200

    Implemented IQueryHandler

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+// Here we have only one interface. As QueryHandler should always return some result.
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull
+{
+}
