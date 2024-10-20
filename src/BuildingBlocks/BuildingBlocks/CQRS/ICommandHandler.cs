171118f97df2702a2d6dfc64ea5780ef5a1e4dd9
Umang Chauhan <umang4191@outlook.com>
2024-10-20 21:54:24 +0200

    Implemented ICommandHandler

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+// Interface for CommandHandler where we do not return any response. For response, we have used Unit.
+public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+}
+
+// Interface for CommandHandler getting the Response object and this response object is not null
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
