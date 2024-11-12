eb6c0d41cf43ddf54092b63f2f65a57d1a4fc0cf
Umang Chauhan <umang4191@outlook.com>
2024-11-12 21:22:48 +0100

    Removed Exception handler lambda from UseExceptionHandler Catalog.API program file

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -27,28 +27,4 @@ var app = builder.Build();
 
 app.MapCarter();
 
-app.UseExceptionHandler(exceptionHandleApp =>
-{
-    exceptionHandleApp.Run(async context =>
-    {
-        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
-        if (exception == null) return;
-
-        var problemDetails = new ProblemDetails
-        {
-            Title = exception.Message,
-            Status = StatusCodes.Status500InternalServerError,
-            Detail = exception.StackTrace
-        };
-
-        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-        logger.LogError(exception, exception.Message);
-
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        context.Response.ContentType = "application/problem+json";
-
-        await context.Response.WriteAsJsonAsync(problemDetails);
-    });
-});
-
 app.Run();
