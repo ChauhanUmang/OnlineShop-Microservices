d0988c18032b326bbe819c04bb2e78411b4bdde0
Umang Chauhan <umang4191@outlook.com>
2024-11-12 21:01:12 +0100

    Added FluentValidation nugets to create CustomExceptionHandler based on IExceptionHandler

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace BuildingBlocks.Exceptions.Handler;
+
+
+// Initially, we can't find IExceptionHanlder. So where is this class? Do we have to install a specific nuget?
+// On checking the documentation for IExceptionHandler, we can see that it comes from Microsoft.AspNetCore.Diagnostics
+// i.e. we need to refer AspNetCore package, but AspNetCore packages are deprecated.
+// they are directly available in the NET SDK, but since this is a class library project, they are not accessible here.
+
+// One approach to fix this is to install FluentValidation nugets (specially FluentValidation.AspNetCore)
+// as they include the ASP.NET libraries.
+public class CustomExceptionHandler : IExceptionHandler
+{
+    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
