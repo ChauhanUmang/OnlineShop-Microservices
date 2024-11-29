516b2d645071c35e649f38b616becb45d551511a
Umang Chauhan <umang4191@outlook.com>
2024-11-29 22:15:41 +0100

    Removed Greeter service and proto file created by default

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,3 @@
-using Discount.Grpc.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,7 +13,7 @@ if (env.IsDevelopment())
     app.MapGrpcReflectionService();
 }
 
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
