7f24369345abd7f97feb40e5763cbaee29387bb3
Umang Chauhan <umang4191@outlook.com>
2024-11-29 22:35:52 +0100

    Added DiscountService

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services;
+
+// To get these classes "DiscountProtoService.DiscountProtoServiceBase", first build the solution after
+// creating the discount.proto file and updating the properties of the discount.proto file
+//Set Build Action = Protobuf compiler
+//gRPC Stub Classes = Server only
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        return base.GetDiscount(request, context);
+    }
+
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        return base.CreateDiscount(request, context);
+    }
+
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        return base.UpdateDiscount(request, context);
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return base.DeleteDiscount(request, context);
+    }
+}
