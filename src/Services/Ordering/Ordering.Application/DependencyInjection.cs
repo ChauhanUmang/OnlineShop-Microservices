fee55f305fac0796d7eede93293999d23a14f199
Umang Chauhan <umang4191@outlook.com>
2024-12-07 14:59:55 +0100

    Added dependency injection between clean architecture layers

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,5 +1,15 @@
-﻿namespace Ordering.Application;
+﻿using Microsoft.Extensions.DependencyInjection;
 
-internal class DependencyInjection
+namespace Ordering.Application;
+
+public static class DependencyInjection
 {
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        //services.AddMediatR(cfg => {
+        //      cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        //});
+
+        return services;
+    }
 }
--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,22 @@
-﻿namespace Ordering.Infrastructure;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-internal class DependencyInjection
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
 {
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DatabaseConnectionString");
+
+        // Add services to the container
+        //services.AddDbContext<ApplicationDbContext>(options => {
+        // options.UseSqlServer(connectionString);
+        //});
+
+        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+        return services;
+    }
 }
