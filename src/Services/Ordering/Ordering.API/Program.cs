90d66fba4f21a3bc72c169bf119d852207b20bc7
Umang Chauhan <umang4191@outlook.com>
2024-12-07 14:29:37 +0100

    Added DependencyInjection class in each project except domain. Will register dependencies in this file instead in program.cs file directly

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -2,6 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 
+//-------------------------
+//Infrastructure - EF Core
+//Application - MediatR
+//API - Carter, HealthChecks, ...
+
+//builder.Services
+//   .AddApplicationServices()
+//   .AddInfrastructureServices(builder.Configuration)
+//   .AddWebServices();
+//--------------------------
+
 var app = builder.Build();
 
 // Configure the HTTP Request pipeline
