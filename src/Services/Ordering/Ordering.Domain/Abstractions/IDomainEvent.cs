c40c3271f185e55e7c780b72ec5dbb2693a30969
Umang Chauhan <umang4191@outlook.com>
2024-12-09 22:31:50 +0100

    Defined Aggregate interface and class

--- src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -6,7 +6,7 @@ namespace Ordering.Domain.Abstractions;
 // We want to inherit from INotification interface from the MediatR to perform domain events. Bcoz MediatR provides
 // very good interface to implement domain event operations inside of the domain layers.
 // for this, we will add Nuget "MediatR" in the domain project.
-internal interface IDomainEvent : INotification
+public interface IDomainEvent : INotification
 {
     // "=>" is called expression-bodied members. This is used to define default values direclty for members of an 
     // interface.
