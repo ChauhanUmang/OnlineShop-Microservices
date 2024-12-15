67b1c8c71d64e7566e6983c96ada45b9f359c1a0
Umang Chauhan <umang4191@outlook.com>
2024-12-15 14:30:03 +0100

    Created seeding data for Customers

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Infrastructure.Data.Extensions;
+
+public class InitialData
+{
+    public static IEnumerable<Customer> Customers =>
+        new List<Customer>
+        {
+            Customer.Create(CustomerId.Of(new Guid("722576ef-ac00-4a8e-808c-c145ef2d8ad0")), "testName1", "test1@gmail.com"),
+            Customer.Create(CustomerId.Of(new Guid("257533ac-afa1-476a-bb2c-d9ca637c550c")), "testName2", "test2@gmail.com" )
+        };
+}
