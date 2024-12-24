4293dd81f3c56a3c0209138af3225fd96761a8e1
Umang Chauhan <umang4191@outlook.com>
2024-12-24 23:04:23 +0100

    Basic setup for CreateOrder command

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+internal class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
+    {
+        // Create order entity from command object
+        // save to database
+        // return result
+
+        throw new NotImplementedException();
+    }
+}
