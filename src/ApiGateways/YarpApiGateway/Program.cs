aa1379560b976dd30d3ff9c222035a9435913ae2
Umang Chauhan <umang4191@outlook.com>
2024-12-28 02:31:27 +0100

    Setup rate limiter on ordering api using yarp

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,12 +1,25 @@
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(rateLimitOptions =>
+{
+    rateLimitOptions.AddFixedWindowLimiter("customPolicy", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(30);
+        options.PermitLimit = 5;
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-app.MapReverseProxy(); ;
+app.UseRateLimiter();
+app.MapReverseProxy();
 
 app.Run();
