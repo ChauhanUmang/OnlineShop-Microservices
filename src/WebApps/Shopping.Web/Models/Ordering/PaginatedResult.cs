e63ec94158caf5b8fcc2994e12d45b863382b127
umangchauhan <umang4191@gmail.com>
2025-03-08 03:22:39 +0100

    Created models for Ordering services in the web client

---
+++ src/WebApps/Shopping.Web/Models/Ordering/PaginatedResult.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Web.Models.Ordering
+{
+    public class PaginatedResult<TEntity>(int pageIndex, int pageSize, long count, IEnumerable<TEntity> data) where TEntity : class
+    {
+        public int PageIndex { get; } = pageIndex;
+        public int PageSize { get; } = pageSize;
+        public long Count { get; } = count;
+        public IEnumerable<TEntity> Data { get; } = data;
+    }
+}
